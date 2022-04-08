9e1323f1fd3e60f5abe9107da27f4f0e80ddf11a
Jia-Baos <18383827268@163.com>
2022-04-08 15:22:48 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+def add(**kwargs):
+    print(type(kwargs))
+    print(kwargs)
+    return kwargs
+
+
+if __name__ == '__main__':
+    my_dict = {'name': 10, 'age': 12}
+    new_dict = add(temperate=10)
+    print(type(new_dict))
+    print(new_dict)
+    my_dict.update(new_dict)
+    print(type(my_dict))
+    print(my_dict)
+
